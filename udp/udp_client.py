763303e0f4c5e9838c7b117d87a22537a3357811
Weibang He <kdlasbang@gmail.com>
2020-02-09 13:15:43 -0800

    finish part(b)

--- udp/udp_client.py
+++ udp/udp_client.py
@@ -2,24 +2,46 @@ import socket
 
 
 UDP_IP = '127.0.0.1'
-UDP_PORT = 4000
+UDP_PORT = 4001
 BUFFER_SIZE = 1024
-MESSAGE = "ping"
+upload=[]
 
+def send():
+    
+    with open("upload.txt","r") as f:
+        data=f.read().splitlines()
 
-def send(id=0):
+    for line in data:
+        words=line.split(':')
+        upload.append(words[1])
     try:
+        ack=0
         s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-        s.sendto(f"{id}:{MESSAGE}".encode(), (UDP_IP, UDP_PORT))
-        data, ip = s.recvfrom(BUFFER_SIZE)
-        print("received data: {}: {}".format(ip, data.decode()))
+        i=0
+        while(i<10000):
+            ack+=len(upload[i])
+            s.sendto(upload[i].encode(), (UDP_IP, UDP_PORT))
+            i+=1
+            if(i%1000==0):
+                data, ip = s.recvfrom(BUFFER_SIZE)
+                if(data.decode()==str(ack)):
+                    print("successfully already send ",i," lines")
+                    s.sendto("SUCCESS!".encode(), (UDP_IP, UDP_PORT))
+                    ack=0
+                    continue
+                else:
+                    s.sendto("RESEND!".encode(), (UDP_IP, UDP_PORT))
+                    i=i-(i%1000)
+                    ack=0
+                    continue
+        print("successfully sent to: {}: {} lines".format(ip, i))
+        
     except socket.error:
         print("Error! {}".format(socket.error))
         exit()
 
 
-def get_client_id():
-    id = input("Enter client id:")
-    return id
+send()
 
-send(get_client_id())
+
+#send(get_client_id())
--- udp/udp_server.py
+++ udp/udp_server.py
@@ -2,20 +2,42 @@ import socket
 
 
 UDP_IP = '127.0.0.1'
-UDP_PORT = 4000
+UDP_PORT = 4001
 BUFFER_SIZE = 1024
-MESSAGE = "pong"
+MESSAGE = "get 1000 lines, checking ack"
 
 def listen_forever():
     s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
     s.bind(("", UDP_PORT))
-
+    package=[]
+    saver=[]
+    check=0
+    i=0
     while True:
         # get the data sent to us
         data, ip = s.recvfrom(BUFFER_SIZE)
-        print("{}: {}".format(ip, data.decode(encoding="utf-8").strip()))
-        # reply back to the client
-        s.sendto(MESSAGE.encode(), ip)
-
-
+        package.append(data.decode(encoding="utf-8"))
+        check+=len(package[i])
+        i+=1
+        if(len(package)==1000):
+            print("{}: {}".format(ip, MESSAGE))
+            # reply back to the client
+            cc=str(check)
+            s.sendto(cc.encode(), ip)
+            data, ip = s.recvfrom(BUFFER_SIZE)
+            if(data.decode(encoding="utf-8")=="RESEND!"):
+                package=[]
+                check=0
+                i=0
+                continue
+            else:
+                saver+=package
+                package=[]
+                check=0
+                i=0
+                if(len(saver)%10000==0):
+                    
+                    print("finish accept 10000 lines from {}".format(ip))
+                continue
+        
 listen_forever()
